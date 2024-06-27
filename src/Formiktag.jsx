eba2fc005baece780e038ef4ce5915719027abcb
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-06-27 16:32:16 +0530

    D

--- src/Formiktag.jsx
+++ src/Formiktag.jsx
@@ -1,54 +1,54 @@
-import { Formik , Form , Field } from 'formik'
+import { Formik, Form, Field } from 'formik'
 import React, { useState } from 'react'
 
 const Formiktag = () => {
-    const [data,setData]=useState([])
-    const handlesubmit = (values)=>{
-        setData([...data,values])
+    const [data, setData] = useState([])
+    const handlesubmit = (values) => {
+        setData([...data, values])
     }
-    const deleteData =(i)=>{
-        let copy=[...data]
-        copy.splice(i,1)
+    const deleteData = (i) => {
+        let copy = [...data]
+        copy.splice(i, 1)
         setData(copy)
     }
-  return (
-    <div>
-      <Formik
-       initialValues={{
-            name:'',
-            id:'',
-            password:''
-    }}
-    onSubmit ={handlesubmit}
-       >
-        <Form>
-            Name :- <Field type="text" name="name" /> <br />
-            Id   :- <Field type="text"  name="id"/> <br />
-            Password :- <Field type="password" name="password" />
-            <br />
-            Enter :- <button type='submit'>Submit</button>
-        </Form>
-      </Formik>
-      <table border={1}>
-        <tr>
-            <th>Name</th>
-            <th>Id</th>
-            <th>Password</th>
-            <th>Delete</th>
-        </tr>
-        {
-            data.map((el,i)=>(
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    name:'',
+                    id: '',
+                    password: ''
+                }}
+                onSubmit={handlesubmit}
+            >
+                <Form>
+                    Name :- <Field type="text" name="name" /> <br />
+                    Id   :- <Field type="text" name="id" /> <br />
+                    Password :- <Field type="password" name="password" />
+                    <br />
+                    Enter :- <button type='submit'>Submit</button>
+                </Form>
+            </Formik>
+            <table border={1}>
                 <tr>
-                    <td>{el.name}</td>
-                    <td>{el.id}</td>
-                    <td>{el.password}</td>
-                    <td><button onClick={()=>deleteData(i)}>Delete</button></td>
+                    <th>Name</th>
+                    <th>Id</th>
+                    <th>Password</th>
+                    <th>Delete</th>
                 </tr>
-            ))    
-        }    
-      </table>
-    </div>
-  )
+                {
+                    data.map((el, i) => (
+                        <tr>
+                            <td>{el.name}</td>
+                            <td>{el.id}</td>
+                            <td>{el.password}</td>
+                            <td><button onClick={() => deleteData(i)}>Delete</button></td>
+                        </tr>
+                    ))
+                }
+            </table>
+        </div>
+    )
 }
 
 export default Formiktag
