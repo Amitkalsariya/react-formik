a3def1405e1a248dce27c9d31cefd06e28b52c86
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-06-28 17:14:37 +0530

    Done

--- src/Formiktag.jsx
+++ src/Formiktag.jsx
@@ -3,23 +3,58 @@ import React, { useState } from 'react'
 
 const Formiktag = () => {
     const [data, setData] = useState([])
-    const handlesubmit = (values) => {
-        setData([...data, values])
+    const [value, setValue] = useState(
+        {
+            name: '',
+            id: '',
+            password: ''
+        }
+    )
+    const [id, setId] = useState(null)
+
+
+    const handlesubmit = (values, { resetForm }) => {
+        if (id !== null) {
+            let copydata = [...data]
+            copydata[id] = values
+            setData(copydata)
+            setId(null)
+
+
+        }
+        else {
+
+            setData([...data, values])
+            resetForm()
+        }
+        setValue({
+            name: '',
+            id: '',
+            password: ''
+
+        })
     }
     const deleteData = (i) => {
         let copy = [...data]
         copy.splice(i, 1)
         setData(copy)
     }
+    const updateData = (el, i) => {
+        setValue({
+            'name': el.name,
+            "id": el.id,
+            'password': el.password
+        })
+        setId(i)
+    }
     return (
         <div>
             <Formik
-                initialValues={{
-                    name:'',
-                    id: '',
-                    password: ''
-                }}
+                enableReinitialize
+                initialValues={value}
                 onSubmit={handlesubmit}
+
+
             >
                 <Form>
                     Name :- <Field type="text" name="name" /> <br />
@@ -35,14 +70,16 @@ const Formiktag = () => {
                     <th>Id</th>
                     <th>Password</th>
                     <th>Delete</th>
+                    <th>Update</th>
                 </tr>
                 {
                     data.map((el, i) => (
-                        <tr>
+                        <tr key={i}>
                             <td>{el.name}</td>
                             <td>{el.id}</td>
                             <td>{el.password}</td>
                             <td><button onClick={() => deleteData(i)}>Delete</button></td>
+                            <td><button onClick={() => updateData(el, i)}>Edit</button></td>
                         </tr>
                     ))
                 }
